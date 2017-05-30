b4cff67069190e3103f4b70f9af7140fd104151e
Eric "ET" Thornton <firmonomics@gmail.com>
2017-05-29 23:56:50 -0400

    Sign in ui custom classes complete

---
+++ devslopes-social/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  devslopes-social
+//
+//  Created by Eric Thornton on 5/29/17.
+//  Copyright © 2017 firmpass llc. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
